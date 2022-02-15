87da6a7167eb7a7e1a6d3ce83ab7f1e08cb6fa3a
Nathan Rodet <rodetnathan.contact@gmail.com>
2022-02-15 14:19:31 +0100

    Updated configuration to copy/paste from solar putty

--- Scripts/conf-server.bash
+++ Scripts/conf-server.bash
@@ -1,8 +1,7 @@
 #!/bin/sh
 #######################################
 
-# Bash script to install an Apache2. For Ubuntu based systems.
-
+# Bash script to install an Apache2 and dependanciesy
 ######################################
 
 ## Update packages and Upgrade system
@@ -14,6 +13,12 @@ sudo apt install php -y
 sudo apt install libapache2-mod-php -y
 sudo apt install mysql-server -y
 sudo apt install php-mysql -y
+## Restart apache2 to apply changes
 sudo /etc/init.d/apache2 restart
 
+## Create Cron to get update for script repository every hour
+sudo crontab -l > cron_gitupdate
+sudo echo "0 * * * * cd /repos/linux-project && git pull" >> cron_gitupdate
+sudo crontab cron_gitupdate
+sudo rm cron_gitupdate
 
